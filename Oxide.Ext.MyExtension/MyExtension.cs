5de5c80faa81ff23a5a43dc8f11d2142e3902288
HandyS11 <valentin-clergue@orange.fr>
2024-01-13 02:51:29 +0100

    Back to .NET Framework 4.8.1 Add extension samples

---
+++ Oxide.Ext.MyExtension/MyExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace Oxide.Ext.MyExtension
+{
+    using System;
+
+    using Oxide.Core;
+    using Oxide.Core.Extensions;
+
+    public class MyExtension(ExtensionManager manager) : Extension(manager)
+    {
+        public override string Name => "<extension_name>";
+        public override string Author => "<author>";
+        public override VersionNumber Version => new(1, 0, 0);
+
+        public override void OnModLoad()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, exception) =>
+            {
+                Interface.Oxide.LogException("An exception was thrown!", exception.ExceptionObject as Exception);
+            };
+        }
+
+        public override void OnShutdown()
+        {
+            // Do something when the server shuts down
+            Interface.Oxide.LogInfo("[MyExtension] has done something");
+        }
+    }
+}
